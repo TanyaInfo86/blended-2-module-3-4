2d0475344245922cec25d1c60648e5327c468d2d
Len4ik <karimovaolena@yahoo.com>
2024-12-22 09:05:06 -0500

    5

--- js/task-6.js
+++ js/task-6.js
@@ -1,2 +1,12 @@
 // Напишіть функцію, яка повертає новий масив без вказаних значень.Використовуйте примітиви
 // Очікується [1, 2, 3, 1, 2] без [1, 2] => [3]
+function removeItems(array1, array2) {
+    const result = [];
+    for (const item of array1) {
+        if (!array2.includes(item)) {
+            result.push(item)
+        }
+    }
+    return result;
+}
+console.log(removeItems([1, 2, 3, 1, 2,5,6], [1, 2]))
