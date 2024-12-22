4e9507934775bb2be4a3b89f28796b1bcd2a6a59
Tetiana Savchuk <tanyainfo86@gmail.com>
2024-12-22 16:33:53 +0200

    task-8

--- js/task-8.js
+++ js/task-8.js
@@ -1,2 +1,11 @@
 // Напишіть функцію, яка розверає масив у зворотньому напрямку.Метод reverse() не використовувати
-// const arrNumber = [ 11, 9, 2, 1 ]
+const arrNumber = [ 11, 9, 2, 1 ]
+function reverseArr(arr){
+    const newArr =[];
+    for (let i =arr.length-1; i>=0; i--){
+        newArr.push(arr[i]);
+    }
+    return newArr;
+}
+console.log(reverseArr(arrNumber))
+console.log(arrNumber.toReversed())
